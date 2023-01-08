1f95f822554855b85ec7ec7186c5b569ca3fe456
Jarrian <glorianagojar@gmail.com>
2023-01-08 12:37:14 +0800

    ADD: inital setup

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
